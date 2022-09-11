0f80890b64f13f67a78032cd12d2f904b3e98797
Ekim Coşkun <108937192+ekimcoskun@users.noreply.github.com>
2022-09-11 22:19:54 +0300

    Add files about promisses class

---
+++ functions/promises.js
@@ -0,0 +1,25 @@
+// Callback
+const getDataCallback = (callback) => {
+    setTimeout(() => {
+        callback(undefined, 'This is the data')
+    }, 2000)
+}
+
+getDataCallback((err, data) => {
+    if(err) {
+
+    }else {
+        console.log(data)
+    }
+})
+
+// Promise
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('This is the promise data')
+    }, 2000)
+})
+
+myPromise.then((data) => {
+    console.log(data)
+})
